c8e25fe9bbfdf5b7b48b50c65a54c9d4eda7e5af
Albert Hakobyan <abhakobyan24@gmail.com>
2021-09-12 01:42:18 +0400

    Добавьте файлы проекта.

---
+++ IsPower_CodeSignal_Core_Test43/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsPower_CodeSignal_Core_Test43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool res = isPower(324);
+            Console.WriteLine(res);
+        }
+
+        static bool isPower(int n)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int j = 2; j < 10; j++)
+                {
+                    if (Math.Pow(i, j) == n)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
